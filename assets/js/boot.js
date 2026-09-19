2ce97078c0b1bd69e2491234eedcc2ee5cd42d58
Robiul Rumman Razim <robiulrummanrazim.facebook@gmail.com>
2026-09-19 23:03:24 +0600

    Load centralized motion core

--- assets/js/boot.js
+++ assets/js/boot.js
@@ -1,4 +1,11 @@
 (function(){
   'use strict';
   document.documentElement.classList.replace('no-js','js');
+
+  window.addEventListener('DOMContentLoaded',function(){
+    var script=document.createElement('script');
+    script.src='./assets/js/motion-core.js?v=20260919-1';
+    script.async=true;
+    document.body.appendChild(script);
+  },{once:true});
 })();
