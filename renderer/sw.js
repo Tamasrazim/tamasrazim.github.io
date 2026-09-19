fe0fa05d8fcc546123da76f0ee6bb6ff74e2964f
Robiul Rumman Razim <robiulrummanrazim.facebook@gmail.com>
2026-09-19 18:36:47 +0600

    Add production animation renderer PWA workspace

---
+++ renderer/sw.js
@@ -0,0 +1,10 @@
+const CACHE='tamasrazim-renderer-v1';
+const CORE=['./','./index.html','./manifest.webmanifest','./icons/icon.svg','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-192-maskable.png','./icons/icon-512-maskable.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  const u=new URL(e.request.url);
+  if(u.origin!==self.location.origin||!u.pathname.startsWith('/renderer/'))return;
+  e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,x));return r}).catch(()=>caches.match('./index.html'))));
+});
