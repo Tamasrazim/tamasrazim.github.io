aed588f162f45f6b1681d1de8c9d7fc8423e63c4
Robiul Rumman Razim <robiulrummanrazim.facebook@gmail.com>
2026-09-20 15:41:24 +0600

    Make BNC AgroCare canonical in main repository

---
+++ projects/bncagrocare/invoice/sw.js
@@ -0,0 +1,10 @@
+const CACHE='bnc-invoice-v11';
+const CORE=['./','./index.html','./css/app.css','./js/app.js','./manifest.webmanifest','./icons/icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  const u=new URL(e.request.url);
+  if(u.origin!==self.location.origin||!u.href.startsWith(self.registration.scope))return;
+  e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{caches.open(CACHE).then(x=>x.put(e.request,r.clone()));return r})));
+});
