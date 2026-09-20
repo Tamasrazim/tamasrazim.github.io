1b93e29d5268cfc3a01d3cbecb778adb157c015b
Robiul Rumman Razim <robiulrummanrazim.facebook@gmail.com>
2026-09-20 13:56:49 +0600

    Bump renderer PWA cache for image layers

--- renderer/sw.js
+++ renderer/sw.js
@@ -1,4 +1,4 @@
-const CACHE='tamasrazim-renderer-v8';
+const CACHE='tamasrazim-renderer-v9';
 const CORE=['./','./index.html','./css/app.css','./js/video-engine.js','./js/app.js','./manifest.webmanifest','./icons/icon.svg','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-192-maskable.png','./icons/icon-512-maskable.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
