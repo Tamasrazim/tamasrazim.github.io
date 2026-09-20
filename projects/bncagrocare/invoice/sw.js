bea14b8bb88b2739e3a7917e7598a946709e1227
Robiul Rumman Razim <robiulrummanrazim.facebook@gmail.com>
2026-09-20 15:54:33 +0600

    Harden canonical BNC invoice offline fallback

--- projects/bncagrocare/invoice/sw.js
+++ projects/bncagrocare/invoice/sw.js
@@ -1,4 +1,4 @@
-const CACHE='bnc-invoice-v11';
+const CACHE='bnc-invoice-v12';
 const CORE=['./','./index.html','./css/app.css','./js/app.js','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
@@ -6,5 +6,5 @@ self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
   const u=new URL(e.request.url);
   if(u.origin!==self.location.origin||!u.href.startsWith(self.registration.scope))return;
-  e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{caches.open(CACHE).then(x=>x.put(e.request,r.clone()));return r})));
+  e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{if(r.ok)caches.open(CACHE).then(x=>x.put(e.request,r.clone()));return r}).catch(()=>caches.match('./index.html'))));
 });
