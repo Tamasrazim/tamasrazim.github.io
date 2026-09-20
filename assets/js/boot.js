1e5ed0e96405888a907fc08c30b612cfc78d23ce
Robiul Rumman Razim <robiulrummanrazim.facebook@gmail.com>
2026-09-20 13:00:43 +0600

    Bust motion core cache after FPS cap

--- assets/js/boot.js
+++ assets/js/boot.js
@@ -4,7 +4,7 @@
 
   window.addEventListener('DOMContentLoaded',function(){
     var script=document.createElement('script');
-    script.src='./assets/js/motion-core.js?v=20260920-2';
+    script.src='./assets/js/motion-core.js?v=20260920-3';
     script.async=true;
     document.body.appendChild(script);
   },{once:true});
