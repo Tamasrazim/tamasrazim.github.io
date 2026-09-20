372e06aed6a8e19a78f39036528846a6a8342010
Robiul Rumman Razim <robiulrummanrazim.facebook@gmail.com>
2026-09-20 16:16:54 +0600

    Bump invoice PWA cache after preview refinements

--- projects/bncagrocare/invoice/sw.js
+++ projects/bncagrocare/invoice/sw.js
@@ -1,4 +1,4 @@
-const CACHE='bnc-invoice-v14';
+const CACHE='bnc-invoice-v15';
 const CORE=['./','./index.html','./css/app.css','./js/app.js','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
