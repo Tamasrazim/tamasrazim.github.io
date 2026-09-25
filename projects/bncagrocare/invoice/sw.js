d57ab55348a1e51628c28b17d5a639a1980fa610
Robiul Rumman Razim <robiulrummanrazim.facebook@gmail.com>
2026-09-26 02:35:01 +0600

    Refresh invoice app cache

--- projects/bncagrocare/invoice/sw.js
+++ projects/bncagrocare/invoice/sw.js
@@ -1,4 +1,4 @@
-const CACHE="bnc-invoice-v4";
+const CACHE="bnc-invoice-v5";
 const CORE=["./","./index.html","./css/app.css","./js/app.js","./manifest.webmanifest","./icons/icon.svg","../reference/products.js","../reference/addProductRow.js","../reference/BNCFINAL.xlsx"];
 const EXCEL="https://cdn.jsdelivr.net/npm/exceljs@4.4.0/dist/exceljs.min.js";
 self.addEventListener("install",e=>e.waitUntil((async()=>{const c=await caches.open(CACHE);await c.addAll(CORE);try{await c.add(EXCEL,await fetch(EXCEL,{mode:"no-cors"}))}catch{}await self.skipWaiting()})()));
