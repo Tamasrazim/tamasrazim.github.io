ed34722dd5c910133e248eb30e00b3961edeaa8d
Robiul Rumman Razim <robiulrummanrazim.facebook@gmail.com>
2026-09-26 02:57:24 +0600

    Bump invoice cache after XLSX app changes

--- projects/bncagrocare/invoice/sw.js
+++ projects/bncagrocare/invoice/sw.js
@@ -1,4 +1,4 @@
-const CACHE="bnc-invoice-v6";
+const CACHE="bnc-invoice-v7";
 const CORE=["./","./index.html","./css/app.css","./js/app.js","./manifest.webmanifest","./icons/icon.svg","../reference/products.js","../reference/addProductRow.js","../reference/BNCFINAL.xlsx"];
 const EXCEL="https://cdn.jsdelivr.net/npm/exceljs@4.4.0/dist/exceljs.min.js";
 
