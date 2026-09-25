bfed08ed2484744791b6b6c971ad06eb8d8cf587
Robiul Rumman Razim <robiulrummanrazim.facebook@gmail.com>
2026-09-26 01:57:21 +0600

    Refresh BNC website PWA cache after redesign

--- projects/bncagrocare/sw.js
+++ projects/bncagrocare/sw.js
@@ -1,8 +1,7 @@
-const CACHE="bnc-public-v1";
+const CACHE="bnc-public-v2";
 const CORE=[
  "./","./index.html","./manifest.webmanifest","./FB_IMG_1789811599210.jpg","./FB_IMG_1789811611633.jpg",
- "./IMG-20260713-WA0000.jpg","./IMG-20260713-WA0001.jpg","./IMG-20260713-WA0002.jpg","./IMG-20260713-WA0003.jpg",
- "./FB_IMG_1789811611633.jpg","./reference/products.js"
+ "./IMG-20260713-WA0000.jpg","./IMG-20260713-WA0001.jpg","./IMG-20260713-WA0002.jpg","./IMG-20260713-WA0003.jpg" ,"./reference/products.js"
 ];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
