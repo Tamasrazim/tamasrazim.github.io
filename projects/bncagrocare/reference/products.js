9d43c9cb7875f0a9d8ba04eaec2b1a4fad00c793
Robiul Rumman Razim <robiulrummanrazim.facebook@gmail.com>
2026-09-25 15:33:38 +0600

    Add shared BNC product reference

---
+++ projects/bncagrocare/reference/products.js
@@ -0,0 +1,12 @@
+window.BNC_PRODUCTS=[
+  {code:"01",name:"NC Gold- 4CPA",packs:["1 Ltr x 12 Bottle","500 ml x 12 Bottle","100 ml x 30 Bottle"]},
+  {code:"02",name:"NC Zinc- Mono 36%",packs:["1kg. x 10 Pack"]},
+  {code:"03",name:"NC Solu- Boron 20%",packs:["500 gm x 10 Pack","100 gm x 24 Pack"]},
+  {code:"04",name:"NC Solu+ Boron 17%",packs:["1kg. x 10 Pack","500 gm x 20 Pack"]},
+  {code:"05",name:"NC Chilli- Chilted Zinc 10%",packs:["500 gm x 10 Pack","100 gm x 30 Pack","17 gm x 100 Pack"]},
+  {code:"06",name:"Pa-Cola- Paclobutazol 25 SC",packs:["20 Ltr","5 Ltr","1 Ltr x 12 Bottle","100 ml x 30 Bottle"]},
+  {code:"07",name:"NC Vit- (NNA 98%)",packs:["1kg. x 10 Pack"]},
+  {code:"08",name:"NC Leaf- GA-3",packs:["10 gm x 100 Pack","1 gm x 100 Pack"]},
+  {code:"09",name:"NC Gyp- Calcium 20% & Sulfur 16%",packs:["10 kg. x 5 Pack","5 kg. x 10 Pack"]},
+  {code:"10",name:"NC-Darma",packs:["500 ml x 12 Bottle","100 ml x 30 Bottle"]}
+];
