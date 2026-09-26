46de12272264a444eb90ed5bba35c4b4cb6bb618
Robiul Rumman Razim <robiulrummanrazim.facebook@gmail.com>
2026-09-26 08:00:03 +0600

    Bump invoice cache after row-engine fix

--- projects/bncagrocare/invoice/sw.js
+++ projects/bncagrocare/invoice/sw.js
@@ -1,4 +1,4 @@
-const CACHE="bnc-invoice-v10";
+const CACHE="bnc-invoice-v11";
 const CORE=["./","./index.html","./css/app.css","./js/app.js","./manifest.webmanifest","./icons/icon.svg","../reference/products.js","../reference/addProductRow.js","../reference/BNCFINAL.xlsx"];
 const EXCEL="https://cdn.jsdelivr.net/npm/exceljs@4.4.0/dist/exceljs.min.js";
 
