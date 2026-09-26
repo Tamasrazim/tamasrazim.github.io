3655c6323fdc6b83332604360c7a2d898c4af525
Robiul Rumman Razim <robiulrummanrazim.facebook@gmail.com>
2026-09-26 08:28:35 +0600

    Refresh invoice cache after preview fix

--- projects/bncagrocare/invoice/sw.js
+++ projects/bncagrocare/invoice/sw.js
@@ -1,4 +1,4 @@
-const CACHE="bnc-invoice-v15";
+const CACHE="bnc-invoice-v16";
 const CORE=["./","./index.html","./css/app.css","./js/app.js","./manifest.webmanifest","./icons/icon.svg","../reference/products.js","../reference/addProductRow.js","../reference/BNCFINAL.xlsx"];
 const EXCEL="https://cdn.jsdelivr.net/npm/exceljs@4.4.0/dist/exceljs.min.js";
 
